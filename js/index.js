3285b3de8e7aa99f97ac5b3a80e184d9cca46f1e
shoumikahmed <shoumikahmed103@gmail.com>
2022-02-28 23:50:28 +0600

    details

--- js/index.js
+++ js/index.js
@@ -38,7 +38,7 @@ const displayPhone = phones => {
             <div class="card-body">
                 <h5 class="card-title">${phone.brand}</h5>
                 <p class="card-text">${phone.phone_name}</p>
-                <button onclick="loadPhoneDetails('${phone.slug}')" type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal"> Details</button>
+                <button onclick="loadPhoneDetails('${phone.slug}')" type="button" class="btn btn-success">Details</button>
             </div>
         </div>`
         searchResult.appendChild(div)
@@ -49,6 +49,22 @@ const loadPhoneDetails = phoneId => {
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data))
+        .then(data => displayPhoneDetails(data.data))
+}
 
+const displayPhoneDetails = phone => {
+    console.log(phone)
+    const phoneDetails = document.getElementById('phone-details')
+    const div = document.createElement('div')
+    // div.classList.add('card')
+    div.innerHTML = `
+    <div class="card" style="width: 13rem;" >
+        <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${phone.brand}</h5>
+            <p class="card-text">${phone.phone_name}</p>
+        </div>
+    </div>
+    `
+    phoneDetails.appendChild(div)
 }

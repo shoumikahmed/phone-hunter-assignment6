43ad1f02c7372eb9bd01fe38cfc0ac2e64376f14
shoumikahmed <shoumikahmed103@gmail.com>
2022-02-28 21:42:03 +0600

    newdiv

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
 const loadPhone = () => {
     const searchField = document.getElementById('search-field')
-    const searchValue = searchField.value
+    const searchText = searchField.value
     searchField.value = ''
+    const error = document.getElementById("error");
+    if (searchText == '') {
+        error.innerText = "please give a phone name";
+        searchField.value = ''
+    }
+    else {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayPhone(data.data))
+    }
+
+}
+
+const displayPhone = phones => {
+    console.log(phones)
 }

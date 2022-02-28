0ac5139f88c0722bc9ecda0fe5dfcd89dbb57dd6
shoumikahmed <shoumikahmed103@gmail.com>
2022-03-01 00:33:21 +0600

    string

--- js/index.js
+++ js/index.js
@@ -55,14 +55,21 @@ const loadPhoneDetails = phoneId => {
 const displayPhoneDetails = phone => {
     console.log(phone)
     const phoneDetails = document.getElementById('phone-details')
+    phoneDetails.textContent = ''
     const div = document.createElement('div')
-    // div.classList.add('card')
+    div.classList.add('col-md-12')
+    div.classList.add('mx-auto')
     div.innerHTML = `
     <div class="card" style="width: 13rem;" >
         <img src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">${phone.brand}</h5>
-            <p class="card-text">${phone.phone_name}</p>
+            <h5 class="card-title">${phone.name}</h5>
+            <p class="card-text">chipset: ${phone.mainFeatures.chipSet}</p>
+            <p class="card-text">display: ${phone.mainFeatures.displaySize}</p>
+            <p class="card-text">memory: ${phone.mainFeatures.memory}</p>
+            <p class="card-text">storage: ${phone.mainFeatures.storage}</p>
+            <p class="card-text">sensors: ${phone.mainFeatures.sensors[0]},${phone.mainFeatures.sensors[1]},${phone.mainFeatures.sensors[2]},${phone.mainFeatures.sensors[3]},${phone.mainFeatures.sensors[4]},${phone.mainFeatures.sensors[5]}</p>
+            <P class="card-title">Bluetooth: ${phone.others.Bluetooth}, GPS: ${phone.others.GPS}, NFC: ${phone.others.NFC}, Radio: ${phone.others.Radio}, USB: ${phone.others.USB}, WLAN: ${phone.others.WLAN}</P>
         </div>
     </div>
     `

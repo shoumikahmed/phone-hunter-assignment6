a2232835fb4bb4462fcc9294b2ec0400ebd5698f
shoumikahmed <shoumikahmed103@gmail.com>
2022-02-28 23:11:10 +0600

    newadd

--- js/index.js
+++ js/index.js
@@ -3,10 +3,13 @@ const loadPhone = () => {
     const searchText = searchField.value
     searchField.value = ''
     const error = document.getElementById("error");
-    if (searchText == '') {
+    if (searchText == '' || isNaN(searchText) == false) {
         error.innerText = "please give a phone name";
         searchField.value = ''
     }
+    // else if (searchText != 'Samsung' || searchText != 'Iphone' || searchText != 'Oppo' || searchText != 'Huawei') {
+    //     return alert('No Result Found')
+    // }
     else {
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
         fetch(url)
@@ -17,14 +20,16 @@ const loadPhone = () => {
 }
 
 const displayPhone = phones => {
+    // console.log(phones)
+
     const searchResult = document.getElementById('search-result')
     searchResult.textContent = ''
-
     if (searchResult == null) {
-        return alert('No Result Found')
+        return alert('please input phone name')
     }
+
     phones.forEach(phone => {
-        console.log(phone)
+        // console.log(phone)
         const div = document.createElement('div')
         div.classList.add('col-md-4')
         div.innerHTML = `
@@ -33,10 +38,17 @@ const displayPhone = phones => {
             <div class="card-body">
                 <h5 class="card-title">${phone.brand}</h5>
                 <p class="card-text">${phone.phone_name}</p>
-                <button type="button" class="btn btn-success">Details</button>
+                <button onclick="loadPhoneDetails('${phone.slug}')" type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal"> Details</button>
             </div>
         </div>`
         searchResult.appendChild(div)
     })
 }
 
+const loadPhoneDetails = phoneId => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data))
+
+}

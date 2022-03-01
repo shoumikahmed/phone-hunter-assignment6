9a65af57d2b2e5d9fc56c416552bac7ddb6db191
shoumikahmed <shoumikahmed103@gmail.com>
2022-03-01 12:07:58 +0600

    end

--- js/index.js
+++ js/index.js
@@ -67,19 +67,18 @@ const displayPhoneDetails = phone => {
     const div = document.createElement('div')
     div.classList.add('col-md-12')
     div.classList.add('col-lg-12')
-    // div.classList.add('mx-auto')
     div.innerHTML = `
-    <div class="card" style="width: 13rem;" >
-        <img src="${phone.image ? phone.image : 'not available'}" class="card-img-top" alt="...">
+    <div class="card"  >
+        <div class=""><img src="${phone.image ? phone.image : 'not available'}" class="" alt=""></div>
         <div class="card-body">
             <h5 class="card-title">${phone.name}</h5>
-            <p class="card-text">chipset: ${phone.mainFeatures.chipSet ? phone.mainFeatures.chipSet : 'not available'}</p>
-            <p class="card-text">display: ${phone.mainFeatures.displaySize ? phone.mainFeatures.displaySize : 'not available'}</p>
-            <p class="card-text">memory: ${phone.mainFeatures.memory ? phone.mainFeatures.memory : 'not available'}</p>
-            <p class="card-text">storage: ${phone.mainFeatures.storage ? phone.mainFeatures.storage : 'not availble'}</p>
-            <p class="card-text">sensors: ${phone.mainFeatures.sensors[0] ? phone.mainFeatures.sensors[0] : 'notavailable'},${phone.mainFeatures.sensors[1] ? phone.mainFeatures.sensors[1] : 'not available'},${phone.mainFeatures.sensors[2] ? phone.mainFeatures.sensors[2] : 'not available'},${phone.mainFeatures.sensors[3] ? phone.mainFeatures.sensors[3] : 'not available'},${phone.mainFeatures.sensors[4] ? phone.mainFeatures.sensors[4] : 'not available'},${phone.mainFeatures.sensors[5] ? phone.mainFeatures.sensors[5] : 'not available'}</p>
-            <P class="card-title">others:{Bluetooth: ${phone.others?.Bluetooth ? phone.others?.Bluetooth : 'not available'}, GPS: ${phone.others?.GPS ? phone.others?.GPS : 'not available'}, NFC: ${phone.others?.NFC ? phone.others?.NFC : 'not available'}, Radio: ${phone.others?.Radio ? phone.others?.Radio : 'not available'}, USB: ${phone.others?.USB ? phone.others?.USB : 'not available'}, WLAN: ${phone.others?.WLAN ? phone.others?.WLAN : 'not available'}}</P>
-            <p class="card-text">ReleaseDate: ${phone.releaseDate ? phone.releaseDate : 'coming soon'}</p>
+            <p class="card-text"><strong>chipset:</strong> ${phone.mainFeatures.chipSet ? phone.mainFeatures.chipSet : 'not available'}</p>
+            <p class="card-text"><strong>display:</strong> ${phone.mainFeatures.displaySize ? phone.mainFeatures.displaySize : 'not available'}</p>
+            <p class="card-text"><strong>memory:</strong> ${phone.mainFeatures.memory ? phone.mainFeatures.memory : 'not available'}</p>
+            <p class="card-text"><strong>storage:</strong> ${phone.mainFeatures.storage ? phone.mainFeatures.storage : 'not availble'}</p>
+            <p class="card-text"><strong>sensor:</strong> ${phone.mainFeatures.sensors[0] ? phone.mainFeatures.sensors[0] : 'notavailable'},${phone.mainFeatures.sensors[1] ? phone.mainFeatures.sensors[1] : 'not available'},${phone.mainFeatures.sensors[2] ? phone.mainFeatures.sensors[2] : 'not available'},${phone.mainFeatures.sensors[3] ? phone.mainFeatures.sensors[3] : 'not available'},${phone.mainFeatures.sensors[4] ? phone.mainFeatures.sensors[4] : 'not available'},${phone.mainFeatures.sensors[5] ? phone.mainFeatures.sensors[5] : 'not available'}</p>
+            <P class="card-title"><strong>other:</strong>{Bluetooth: ${phone.others?.Bluetooth ? phone.others?.Bluetooth : 'not available'}, GPS: ${phone.others?.GPS ? phone.others?.GPS : 'not available'}, NFC: ${phone.others?.NFC ? phone.others?.NFC : 'not available'}, Radio: ${phone.others?.Radio ? phone.others?.Radio : 'not available'}, USB: ${phone.others?.USB ? phone.others?.USB : 'not available'}, WLAN: ${phone.others?.WLAN ? phone.others?.WLAN : 'not available'}}</P>
+            <p class="card-text"><strong>Release Date:</strong> ${phone.releaseDate ? phone.releaseDate : 'coming soon'}</p>
         </div>
     </div>
     `
